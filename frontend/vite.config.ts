4dbaf34f79219610f741342091f879d705dd13f0
vmilard <vmilard@mailinblack.com>
2026-03-10 19:19:09 +0100

    ajout courbe media cloud

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: mode === 'production' ? '/presidentielle2027/' : '/',
-}))
+  base: '/',
+})
